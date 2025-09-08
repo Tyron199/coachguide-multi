794e58a14a223d0fe9d9ae69b0604283d43f3af6
Tyron Streaton <tyron.streaton@gmail.com>
2025-09-08 13:42:11 -0600

    Update

---
+++ resources/js/pages/Tenant/admin/subscription/ManageSubscription.vue
@@ -0,0 +1,23 @@
+<template>
+    <AppLayout :breadcrumbs="breadcrumbItems">
+
+        <Head title="Manage Subscription" />
+
+        <BillingLayout>
+
+        </BillingLayout>
+    </AppLayout>
+</template>
+
+<script setup lang="ts">
+import AppLayout from '@/layouts/AppLayout.vue';
+import BillingLayout from '@/layouts/billing/Layout.vue';
+import { BreadcrumbItem } from '@/types';
+import { manage } from '@/actions/App/Http/Controllers/Tenant/Admin/SubscriptionController';
+const breadcrumbItems: BreadcrumbItem[] = [
+    {
+        title: 'Subscription',
+        href: manage().url,
+    }
+];
+</script>
---
+++ resources/js/pages/Tenant/admin/subscription/Receipt.vue
@@ -0,0 +1,31 @@
+<template>
+    <AppLayout :breadcrumbs="breadcrumbItems">
+
+        <Head title="Subscribe" />
+
+        <BillingLayout>
+            <template #header>
+                <h1 class="text-2xl font-semibold text-foreground">Subscribe</h1>
+                <p class="text-muted-foreground">Subscribe to the platform to continue using it.</p>
+            </template>
+        </BillingLayout>
+    </AppLayout>
+</template>
+
+<script setup lang="ts">
+import AppLayout from '@/layouts/AppLayout.vue';
+import BillingLayout from '@/layouts/billing/Layout.vue';
+import { BreadcrumbItem } from '@/types';
+import { manage, receipt } from '@/actions/App/Http/Controllers/Tenant/Admin/SubscriptionController';
+
+const breadcrumbItems: BreadcrumbItem[] = [
+    {
+        title: 'Subscription',
+        href: manage().url,
+    },
+    {
+        title: 'Receipt',
+        href: receipt().url,
+    },
+];
+</script>
